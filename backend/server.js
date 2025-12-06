9cece31d2d6092ccde08b7939b859b8ca5bc5a45
syed-waleed-ahmed <syedwaleedahmed9@gmail.com>
2025-12-06 03:20:13 +0100

    updated origins

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // ===== Middleware =====
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://your-frontend-domain.netlify.app", // we'll update this after first deploy
+  "https://portfoliofrontendd.netlify.app", 
 ];
 
 app.use(
